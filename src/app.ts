af080d1af93a7e8ad75e8c7ade3738c620b921d7
TheoMa76 <tmaerten@normandiewebschool.fr>
2025-05-06 15:23:24 +0200

    cors

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,19 @@ import answerRoutes from './routes/answer.routes';
 const app = express();
 
 app.use(express.json());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  };
+  
+app.use(cors(corsOptions));
 app.use('/questions',questionRoutes);
 app.use('/answers', answerRoutes);
 
 export default app;
+function cors(corsOptions: { origin: string; methods: string[]; allowedHeaders: string[]; credentials: boolean; }): any {
+    throw new Error('Function not implemented.');
+}
+
